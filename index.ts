6c63a39ca3bd553bb0e6e80c65efad32099ea976
Seth Doty <seth.doty@protonmail.com>
2019-11-10 21:03:37 -0600

    Documentation updates

--- index.ts
+++ index.ts
@@ -21,8 +21,10 @@ const bucket = new aws.s3.Bucket("seth-doty-opi-pulumi-bucket", {
 // Export the name of the bucket
 export const bucketName = bucket.id;
 
+// Create a SNS Topic
 const demoTopic = new aws.sns.Topic("seth-pulumi-demo-topic", {});
 
+//Connect a bucket notification to our SNS topic
 const bucketNotification = new aws.s3.BucketNotification("eventNotification", {
     bucket: bucket.id,
     topics: [{
@@ -31,20 +33,23 @@ const bucketNotification = new aws.s3.BucketNotification("eventNotification", {
     }]
 })
 
+//Initialize the pulumi Configuration
 const cfg = new pulumi.Config()
+// Configure a reference to your message value in our pulumi Configuration.  Created with `pulumi config set --secret secretMessage "Look mom, I'm a secret"`
 const secretMessage = cfg.requireSecret("secretMessage")
-
+// Add the value of secretMessage to aws SSM
 const secretParam = new aws.ssm.Parameter("secretParameter", {
     type: "SecureString",
     value: secretMessage.apply(s => s.toUpperCase())
 });
 
+//Export these values from our state during the pulumi up command
 exports.topicArn = demoTopic.arn
 exports.kmsarn = key.arn
 exports.secretId = secretParam.id
 exports.secretValue = secretParam.value
 
-//I like Magic Functions More
+//I like Magic Functions More Lets make one of those
 
 // Create our bucket.
 const docsBucket = new aws.s3.Bucket("seth-doty-opi-pulumi-magic-bucket", {
